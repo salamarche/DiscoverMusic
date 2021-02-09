f83e2064f95adb216734f681aebc37fa3798b972
Simone LaMarche <salamarche@madisoncollege.edu>
2021-02-09 10:37:57 -0600

    adding all

--- src/main/java/edu/matc/entity/User.java
+++ src/main/java/edu/matc/entity/User.java
@@ -1,2 +1,15 @@
-package edu.matc.entity;public class User {
+package edu.matc.entity;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "User") //class name
+@Table(name = "user") //table name
+
+public class User {
+
+    @Column(name = "user_name") //this does not have to be included if the column name is the same as var
+    private String userName;
+
+
+
 }
