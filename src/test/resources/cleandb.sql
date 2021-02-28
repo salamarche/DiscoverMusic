029c6c70d1a88cf0cb3f3c9f5dc5ae0cce77177a
Simone LaMarche <salamarche@madisoncollege.edu>
2021-02-28 13:53:40 -0600

    updated cleandb to included additional engagements for testing

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -3,4 +3,4 @@ DELETE FROM artist;
 DELETE FROM user;
 INSERT INTO user VALUES (1,'jcoyne','test@test.test','supersecret1'),(2,'fhensen','test@test.test','supersecret2'),(3,'bcurry','test@test.test','supersecret3'),(4,'kmack','test@test.test','supersecret4'),(5,'dklein','test@test.test','supersecret5'),(6,'dtillman','test@test.test','supersecret6');
 INSERT INTO artist VALUES (1, 'soundcloudId100', 'DJ Test', 'some location'), (2, 'soundcloudId200', 'Ipsum Lorem', 'some other location');
-INSERT INTO artist_engagement VALUES (1, 1, '2020-01-01 10:10:10'), (2, 1, '2020-01-01 10:10:10');
+INSERT INTO artist_engagement VALUES (1, 1, '2020-01-01 10:10:10'), (2, 1, '2020-01-01 10:10:10'), (1, 2, '2020-01-01 10:10:10'), (1, 3, '2020-01-01 10:10:10');
