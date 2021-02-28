8fef381ac05724d3f0a1214e6e6b6d6175a511bc
Simone LaMarche <salamarche@madisoncollege.edu>
2021-02-28 13:53:06 -0600

    added specialized ArtistEngagementDao for retrieving list of artist engaments,fully tested

--- src/main/java/edu/matc/persistence/ArtistEngagementDao.java
+++ src/main/java/edu/matc/persistence/ArtistEngagementDao.java
@@ -1,2 +1,48 @@
-package edu.matc.persistence;public class ArtistEngagementDao {
+package edu.matc.persistence;
+
+import edu.matc.entity.Artist;
+import edu.matc.entity.ArtistEngagement;
+import edu.matc.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.*;
+
+public class ArtistEngagementDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public ArtistEngagementDao() {}
+
+    public List<ArtistEngagement> getArtistEngagementByArtist(Artist artist) {
+        Session session = sessionFactory.openSession();
+        String propertyName = "artist";
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<ArtistEngagement> query = builder.createQuery( ArtistEngagement.class );
+        Root<ArtistEngagement> root = query.from( ArtistEngagement.class );
+        query.select(root).where(builder.equal(root.get(propertyName), artist));
+        List<ArtistEngagement> artistEngagementList = session.createQuery( query ).getResultList();
+
+        return artistEngagementList;
+    }
+
+    public List<ArtistEngagement> getArtistEngagementByUser(User user) {
+        Session session = sessionFactory.openSession();
+        String propertyName = "user";
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<ArtistEngagement> query = builder.createQuery( ArtistEngagement.class );
+        Root<ArtistEngagement> root = query.from( ArtistEngagement.class );
+        query.select(root).where(builder.equal(root.get(propertyName), user));
+        List<ArtistEngagement> artistEngagementList = session.createQuery( query ).getResultList();
+
+        return artistEngagementList;
+    }
+
 }
--- src/test/java/edu/matc/persistence/ArtistEngagementDaoTest.java
+++ src/test/java/edu/matc/persistence/ArtistEngagementDaoTest.java
@@ -1,4 +1,45 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.Artist;
+import edu.matc.entity.ArtistEngagement;
+import edu.matc.entity.User;
+import edu.matc.test.util.Database;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ArtistEngagementDaoTest {
-  
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    ArtistEngagementDao artistEngagementDao;
+    GenericDao<Artist> artistDao;
+    GenericDao<User> userDao;
+
+    @BeforeEach
+    void setUp() {
+        artistEngagementDao = new ArtistEngagementDao();
+        artistDao = new GenericDao(Artist.class);
+        userDao = new GenericDao<>(User.class);
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
+
+    @Test
+    void getArtistEngagementByArtist() {
+        Artist artist = (Artist)artistDao.getById(1);
+        List<ArtistEngagement> artistEngagementList = artistEngagementDao.getArtistEngagementByArtist(artist);
+        assert(artistEngagementList.size() == 3);
+
+    }
+
+    @Test
+    void getArtistEngagementByUser() {
+        User user = (User)userDao.getById(1);
+        List<ArtistEngagement> artistEngagementList = artistEngagementDao.getArtistEngagementByUser(user);
+        assert(artistEngagementList.size() == 2);
+    }
 }
