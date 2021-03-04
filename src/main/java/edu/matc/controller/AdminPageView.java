dedfe832cbc58e1afa78e4cf63337bf97fc2449f
Simone LaMarche <salamarche@madisoncollege.edu>
2021-03-04 09:26:20 -0600

    adminPageView controller

--- src/main/java/edu/matc/controller/AdminPageView.java
+++ src/main/java/edu/matc/controller/AdminPageView.java
@@ -1,2 +1,33 @@
-package edu.matc.controller;public class AdminPageView {
+package edu.matc.controller;
+
+
+import edu.matc.entity.Artist;
+import edu.matc.entity.User;
+import edu.matc.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/admin"}
+)
+public class AdminPageView extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        GenericDao userDao = new GenericDao(User.class);
+        req.setAttribute("users", userDao.getAll());
+
+        GenericDao artistDao = new GenericDao(Artist.class);
+        req.setAttribute("artists", artistDao.getAll());
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/admin.jsp");
+        dispatcher.forward(req, resp);
+    }
+
 }
