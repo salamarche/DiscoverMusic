5b9cc3c1b7ca0ea03ea748252070d35ac0b9c3dd
Simone LaMarche <salamarche@madisoncollege.edu>
2021-03-28 11:48:52 -0500

    added a rest service class

--- src/main/java/edu/matc/restservice/ArtistService.java
+++ src/main/java/edu/matc/restservice/ArtistService.java
@@ -1,2 +1,19 @@
-package edu.matc.restservice;public class ArtistService {
+package edu.matc.restservice;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/artists")
+public class ArtistService {
+    @GET
+    @Produces("text/plain")
+    public Response getArtists() {
+        String output = null;
+        return Response.status(200).entity(output).build();
+    }
+
 }
+
+
+
