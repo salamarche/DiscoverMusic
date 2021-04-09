1ad8166f4b20d9863e7387c0f0a385f60fdbe97c
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-09 15:26:44 -0500

    new db schema and sample data

--- DesignDocuments/createTables.sql
+++ DesignDocuments/createTables.sql
@@ -1,16 +1,53 @@
+DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS region;
+DROP TABLE IF EXISTS city;
+
 DROP TABLE IF EXISTS artist_engagement;
 DROP TABLE IF EXISTS artist;
 DROP TABLE IF EXISTS user;
 
+/* LOCATIONS */
+create table country (
+                         id INT primary key,
+                         countryName varchar(100),
+                         iso3 char(3)
+);
+
+create table region (
+                        id INT primary key,
+                        regionName varchar(100),
+                        countryId int
+);
+
+create table city (
+                      id INT primary key,
+                      cityName varchar(100),
+                      lat varchar(20),
+                      lng varchar(20),
+                      regionId int
+);
+
+ALTER TABLE region
+    ADD constraint fk_countryId
+        foreign key (countryId) references country(id);
+
+ALTER TABLE city
+    add constraint fk_regionId
+        foreign key (regionId) references region(id);
+
+/* ARTISTS */
 create table artist
 (
     id            int auto_increment
         primary key,
-    soundcloud_id varchar(100) null,
+    spotify_id varchar(100) null,
     artist_name   varchar(100) null,
-    location      varchar(100) null,
+    city_id       int null,
     avatar_url    varchar(500) null,
-    description   varchar(500) null
+    description   varchar(500) null,
+
+    constraint FK_location_id
+        foreign key (city_id) references city (id)
 
 );
 
@@ -20,7 +57,6 @@ create table user
         primary key,
     user_name varchar(100) not null,
     email     varchar(100) not null,
-    password  char(64)     not null,
     user_role varchar(100) not null
 );
 
--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,6 +1,21 @@
+DELETE FROM city;
+DELETE FROM region;
+DELETE FROM country;
+
 DELETE FROM artist_engagement;
 DELETE FROM artist;
 DELETE FROM user;
-INSERT INTO user VALUES (1,'jcoyne','test@test.test','supersecret1','user'),(2,'fhensen','test@test.test','supersecret2','user'),(3,'bcurry','test@test.test','supersecret3','user'),(4,'kmack','test@test.test','supersecret4','user'),(5,'dklein','test@test.test','supersecret5','user'),(6,'dtillman','test@test.test','supersecret6','user');
-INSERT INTO artist VALUES (1, 'soundcloudId100','DJ Test','some location','photo-url','artist description'), (2, 'soundcloudId200', 'Ipsum Lorem', 'some other location','photo-url','artist description');
+
+
+INSERT INTO country VALUES (1, 'United States', 'USA');
+INSERT INTO country VALUES (2, 'Canada', 'CAN');
+INSERT INTO region VALUES (1, 'Wisconsin', 1);
+INSERT INTO region VALUES (2, 'New York', 1);
+INSERT INTO region VALUES (3, 'Quebec', 2);
+INSERT INTO city VALUES (1, 'Montreal', 'latcoord', 'lngcoord', 3);
+INSERT INTO city VALUES (2, 'Madison', 'latcoord', 'longcoord', 1);
+
+
+INSERT INTO user VALUES (1,'jcoyne','test@test.test','user'),(2,'fhensen','test@test.test','user'),(3,'bcurry','test@test.test', 'user'),(4,'kmack','test@test.test','user'),(5,'dklein','test@test.test','supersecret5','user'),(6,'dtillman','test@test.test','supersecret6','user');
+INSERT INTO artist VALUES (1, 'soundcloudId100','DJ Test',1,'photo-url','artist description'), (2, 'soundcloudId200', 'Ipsum Lorem', 2,'photo-url','artist description');
 INSERT INTO artist_engagement VALUES (1, 1, '2020-01-01 10:10:10'), (2, 1, '2020-01-01 10:10:10'), (1, 2, '2020-01-01 10:10:10'), (1, 3, '2020-01-01 10:10:10');
