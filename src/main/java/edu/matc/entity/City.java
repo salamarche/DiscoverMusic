9c9344760b6cea7a70985c06529b50c38e59c4f8
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-09 16:06:42 -0500

    added city entity

--- src/main/java/edu/matc/entity/City.java
+++ src/main/java/edu/matc/entity/City.java
@@ -1,2 +1,66 @@
-package edu.matc.entity;public class City {
+package edu.matc.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "City")
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    private String cityName;
+
+
+    private String lat;
+
+
+    private String lng;
+
+    @ManyToOne
+    @JoinColumn(name="regionId", nullable = false)
+    private Region region;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
 }
