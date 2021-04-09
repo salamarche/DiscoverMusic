7e8565ebaa2ec62a896c9479ed1cc17b2ca6b33c
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-09 16:50:28 -0500

    first test passing

--- src/test/java/edu/matc/persistence/CountryTest.java
+++ src/test/java/edu/matc/persistence/CountryTest.java
@@ -1,2 +1,48 @@
-package edu.matc.persistence;public class CountryTest {
+package edu.matc.persistence;
+
+import edu.matc.entity.ArtistEngagement;
+import edu.matc.entity.Country;
+import edu.matc.test.util.Database;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CountryTest {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
+    GenericDao countryDao;
+
+    @BeforeEach
+    void setUp() {
+        countryDao = new GenericDao(Country.class);
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
+
+    @Test
+    void getByIdSuccess() {
+        Country retrievedCountry = (Country)countryDao.getById(1);
+        assertNotNull(retrievedCountry);
+        assert(retrievedCountry.getCountryName().equals("United States"));
+
+    }
+
+    @Test
+    void getByPropertyEqualSuccess() {
+
+
+    }
+
+    @Test
+    void getAll() {
+
+    }
+
 }
