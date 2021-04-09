53c604b8fce43e0cee78b933a33ce0ea7bff18e4
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-09 15:41:49 -0500

    added Country entity

--- src/main/java/edu/matc/entity/Country.java
+++ src/main/java/edu/matc/entity/Country.java
@@ -1,2 +1,48 @@
-package edu.matc.entity;public class Country {
+package edu.matc.entity;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "Country")
+@Table(name = "country")
+
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "countryName")
+    private String countryName;
+
+    @Column(name = "iso3")
+    private String iso3;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getIso3() {
+        return iso3;
+    }
+
+    public void setIso3(String iso3) {
+        this.iso3 = iso3;
+    }
 }
