a1c3db02187c44c9b0fa5b25251c73fdf3bcaeae
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-09 18:27:33 -0500

    all get tests passing for country

--- src/test/java/edu/matc/persistence/CountryTest.java
+++ src/test/java/edu/matc/persistence/CountryTest.java
@@ -2,6 +2,7 @@ package edu.matc.persistence;
 
 import edu.matc.entity.ArtistEngagement;
 import edu.matc.entity.Country;
+import edu.matc.entity.Region;
 import edu.matc.test.util.Database;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -18,10 +20,12 @@ public class CountryTest {
 
 
     GenericDao countryDao;
+    GenericDao regionDao;
 
     @BeforeEach
     void setUp() {
         countryDao = new GenericDao(Country.class);
+        regionDao = new GenericDao(Region.class);
         Database database = Database.getInstance();
         database.runSQL("cleandb.sql");
     }
@@ -36,13 +40,26 @@ public class CountryTest {
 
     @Test
     void getByPropertyEqualSuccess() {
-
+        String propertyName = "iso3";
+        String value = "USA";
+        List<Country> countries = countryDao.getByPropertyEqual(propertyName, value);
+        assert(countries.size() == 1 );
 
     }
 
     @Test
     void getAll() {
 
+        List<Country> allCountries = countryDao.getAll();
+        assert(allCountries.size() == 2);
+
+    }
+
+    @Test
+    void getAllRegions() {
+        Country country = (Country)countryDao.getById(1);
+        Set<Region> regions = country.getRegions();
+        assert(regions.size() == 2);
     }
 
 }
