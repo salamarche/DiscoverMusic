a624473a4f520a5c29de7aba6058b31ee41983c3
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-15 19:19:55 -0500

    testing data list

---
+++ src/main/java/edu/matc/controller/GetLocationPageView.java
@@ -0,0 +1,33 @@
+package edu.matc.controller;
+
+import edu.matc.entity.Country;
+import edu.matc.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/location"}
+)
+
+public class GetLocationPageView extends HttpServlet {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        GenericDao countryDao = new GenericDao(Country.class);
+        req.setAttribute("countries", countryDao.getAll());
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/getLocation.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+}
