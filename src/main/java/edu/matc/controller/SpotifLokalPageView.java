a361fac5ce374a5e909cda3273b5c815dd3eebf9
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-19 13:47:31 -0500

    maing page elments in

--- src/main/java/edu/matc/controller/SpotifLokalPageView.java
+++ src/main/java/edu/matc/controller/SpotifLokalPageView.java
@@ -1,7 +1,5 @@
 package edu.matc.controller;
 
-import edu.matc.entity.Country;
-import edu.matc.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,19 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(
-        urlPatterns = {"/location"}
+        urlPatterns = {"/spotifloKation"}
 )
 
-public class GetLocationPageView extends HttpServlet {
+public class LocalifyPageView extends HttpServlet {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        GenericDao countryDao = new GenericDao(Country.class);
-        req.setAttribute("countries", countryDao.getAll());
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/locationFormElements.jsp");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/localify.jsp");
         dispatcher.forward(req, resp);
     }
 
