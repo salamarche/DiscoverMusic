e4b65208e2bddd4f6459cafd63eeb631109ba900
Simone LaMarche <salamarche@madisoncollege.edu>
2021-04-20 16:24:55 -0500

    added in check for existing engagement before adding one

--- src/main/java/edu/matc/controller/DiscoverFavorite.java
+++ src/main/java/edu/matc/controller/DiscoverFavorite.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @WebServlet(
         urlPatterns = {"/favorite"}
@@ -41,13 +42,28 @@ public class DiscoverFavorite extends HttpServlet {
             int artistId = Integer.parseInt(req.getParameter("artistId"));
             Artist artist = (Artist) artistDao.getById(artistId);
 
+            //check for existing artist engagement before adding it.
+            Set<ArtistEngagement> engagements = user.getArtistUserEngagement();
+            Boolean artistFound = false;
+
+            for (ArtistEngagement engagement : engagements) {
+                Artist thisArtist = engagement.getArtist();
+                if (thisArtist.getId() == artistId ) {
+                    artistFound = true;
+                    break;
+                }
+            }
+
+            if (artistFound == false) {
+                LocalDateTime dateTime = LocalDateTime.now();
+                ArtistEngagement engagement = new ArtistEngagement(artist, user, dateTime);
+                artistEngagementDao.insert(engagement);
+            }
+
+
+
+            //return to previous page
             String cityId = req.getParameter("cityId");
-
-            LocalDateTime dateTime = LocalDateTime.now();
-            ArtistEngagement engagement = new ArtistEngagement(artist, user, dateTime);
-            artistEngagementDao.insert(engagement);
-
-
             url = "./discoverAction?selectedCityId=" + cityId;
 
         } else {
