87c189d29c55b6737e404434c48db0310111b6a8
Simone LaMarche <salamarche@madisoncollege.edu>
2021-05-08 10:01:57 -0500

    comments

--- src/main/java/edu/matc/controller/LoginPageView.java
+++ src/main/java/edu/matc/controller/LoginPageView.java
@@ -22,8 +22,8 @@ public class LoginPageView extends HttpServlet implements PropertiesLoader {
 
         //TODO: change for deployment
         //Local
-
         String url = "https://discover-music.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=4lqlga33rukqfjnas0rbi1rnn4&redirect_uri=http://localhost:8080/DiscoverMusic_war/confirmation";
+
         //Aws - This won't work because it won't take http:// protocol, or a straight ip address
         //String url = "https://discover-music.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=4lqlga33rukqfjnas0rbi1rnn4&redirect_uri=3.140.97.66:8080/DiscoverMusic/confirmation";
 
