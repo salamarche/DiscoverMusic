d29fb5395108c9466bb7df90ab26a9f281f3f7a6
Simone LaMarche <salamarche@madisoncollege.edu>
2021-05-09 18:01:35 -0500

    javadoc comments utilities

--- src/main/java/edu/matc/utilities/PropertiesLoader.java
+++ src/main/java/edu/matc/utilities/PropertiesLoader.java
@@ -3,8 +3,17 @@ package edu.matc.utilities;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Properties Loader interface
+ */
 public interface PropertiesLoader {
 
+    /**
+     * Loads in a properties file and returns properties
+     * @param propertiesFilePath file path location of properties
+     * @return properties
+     * @throws Exception
+     */
     default Properties loadProperties(String propertiesFilePath) throws Exception {
         Properties properties = new Properties();
         try {
